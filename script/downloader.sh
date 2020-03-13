4c344ad9ff298c7503e996b0cc1a8d129bf4d496
Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>
2020-03-13 21:13:59 +0330

    add: bash script to download icons from github

---
+++ script/downloader.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+CURRDIR="$(echo $PWD)"
+ICONDIR="$(echo ${PWD%/*})/kiasimcons/icons"
+TEMPDIR="temp"
+TEMPICON="$( echo "$TEMPDIR/"*"/icons" )"
+
+LOCATION=$(curl -s https://api.github.com/repos/simple-icons/simple-icons/releases/latest \
+| grep "tag_name" \
+| awk '{print "https://github.com/simple-icons/simple-icons/archive/" substr($2, 2, length($2)-3) ".tar.gz"}') \
+
+mkdir -p $TEMPDIR && curl -L $LOCATION | tar -xzv --directory $TEMPDIR
+
+rm -rf "$ICONDIR" && mkdir -p "$ICONDIR"
+
+mv -v  $TEMPICON/* "$ICONDIR/" && rm -rf $TEMPDIR
