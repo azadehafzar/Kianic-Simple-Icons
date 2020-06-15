03f15dd9b193220db16fdfb9e13a54f3d664ec1a
Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>
2020-06-15 18:07:12 +0430

    modify: don't pipe curl into tar

--- script/downloader.sh
+++ script/downloader.sh
@@ -2,15 +2,33 @@
 
 CURRDIR="$PWD"
 ICONDIR="${PWD%/*}/kiasimcons/icons"
+ICON_TARGZ="kiasimcons.tar.gz"
 TEMPDIR="temp"
 TEMPICON="$( echo "$TEMPDIR/"*"/icons" )"
 
+# Create temporary directory.
+mkdir -p $TEMPDIR 
+
+# Reset current icon directories.
+rm -rf "$ICONDIR"
+mkdir -p "$ICONDIR"
+
+# Create download link.
 LOCATION=$(curl -s https://api.github.com/repos/simple-icons/simple-icons/releases/latest \
 | grep "tag_name" \
 | awk '{print "https://github.com/simple-icons/simple-icons/archive/" substr($2, 2, length($2)-3) ".tar.gz"}') \
 
-mkdir -p $TEMPDIR && curl -L $LOCATION | tar -xz --directory $TEMPDIR
+# Download.
+curl -L "$LOCATION" > "$ICON_TARGZ"
 
-rm -rf "$ICONDIR" && mkdir -p "$ICONDIR"
+# Extract
+tar -xz --file "$ICON_TARGZ" --directory "./$TEMPDIR"
 
-mv $TEMPICON/* "$ICONDIR/" && rm -rf $TEMPDIR
+# Delete downloaded tar.gz
+rm "$ICON_TARGZ"
+
+# Move files from temporary directory to main directory.
+mv $TEMPICON/* "$ICONDIR/"
+
+# Delete temporary folder.
+rm -rf $TEMPDIR
